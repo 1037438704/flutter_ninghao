415d6556e19a0664258546c9aed08487ed864f68
NineTailedFox <1037438704@qq.com>
2020-12-04 10:19:27 +0800

    重新提交

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,19 @@
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_ninghao/pages/home_page.dart';
-import 'package:flutter_ninghao/route/application.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final router = FluroRouter();
-    var usersHandler = Handler(handlerFunc: (BuildContext context, Map<String, dynamic> params) {
-      return HomePage();
-    });
-
-    void defineRoutes(FluroRouter router) {
-      router.define("/users/:id", handler: usersHandler);
-    }
-
-
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Fish Router',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: Center(),
     );
   }
 }
